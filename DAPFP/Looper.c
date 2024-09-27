5fd70554073c77e92efabb4d80c786c497beae8b
Thomas DiModica <ricinwich@yahoo.com>
2024-09-26 22:34:03 -0600

    Even if you don't use the exponential function, the accumulated rounding errors will destroy you.

---
+++ DAPFP/Looper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+ {
+   int i;
+   printf("2 length 19800\n");
+   for (i = 0; i < 65536; ++i)
+    {
+      printf("copy *\n");
+    }
+   printf("print\n");
+   printf("exit\n");
+   return 0;
+ }
